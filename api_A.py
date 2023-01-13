fd128a3f6f88256e81ecd374730339cffec97f61
Evan Guma <eguma@edtechlab.africa>
2023-01-13 17:27:24 +0300

    'login_auth'

--- api_A.py
+++ api_A.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
 
 app = Flask(__name__)
 
@@ -94,6 +96,23 @@ def delete_user(name):
 	db.session.commit()
 
 	return jsonify({'message' : 'User has been deleted!'})
+
+@app.route('/login')
+def login():
+	auth = request.authorization
+
+	if not auth or not auth.username or not auth.password:
+		return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required"'})
+
+	user = User.query.filter_by(name=auth.username).first()
+
+	if not user:
+		return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required"'})
+
+	if check_password_hash(user.password, auth.password):
+		token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelata(minutes=30)}, app.config['SECRET_KEY'])
+
+		return jsonify({'token' : token.decode('UTF-8')})
 
 if __name__ == '__main__':
 	app.run(debug=True)
