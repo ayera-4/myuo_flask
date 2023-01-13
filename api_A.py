a2ebc394fdcb9ee64939a52ea821d3b016552605
Evan Guma <eguma@edtechlab.africa>
2023-01-13 16:43:27 +0300

    'Promote_user'

--- api_A.py
+++ api_A.py
@@ -41,9 +41,21 @@ def get_all_users():
 
 	return jsonify({'users':output})
 
-@app.route('/user/<user_id>', methods=['GET'])
-def get_one_user():
-	return ''
+@app.route('/user/<public_id>', methods=['GET'])
+def get_one_user(public_id):
+
+	user = User.query.filter_by(public_id=public_id).first()
+
+	if not user:
+		return jsonify({'message' : 'No user found!'})
+
+	user_data = {}
+	user_data['public_id'] = user.public_id
+	user_data['name'] = user.name
+	user_data['password'] = user.password
+	user_data['admin'] = user.admin
+
+	return jsonify({'user' : user_data})
 
 @app.route('/user', methods=['POST'])
 def create_user():
@@ -57,13 +69,31 @@ def create_user():
 
 	return jsonify({'message' : 'New user created!'})
 
-@app.route('/user/<user_id>', methods=['PUT'])
-def promote_user():
-	return ''
+@app.route('/user/<name>', methods=['PUT'])
+def promote_user(name):
 
-@app.route('/user/<user_id>', methods=['DELETE'])
-def delete_user():
-	return ''
+	user = User.query.filter_by(name=name).first()
+
+	if not user:
+		return jsonify({'message' : 'No user found!'})
+
+	user.admin = True
+	db.session.commit()
+
+	return jsonify({'message' : 'User has been promoted!'})
+
+
+@app.route('/user/<name>', methods=['DELETE'])
+def delete_user(name):
+	user = User.query.filter_by(name=name).first()
+
+	if not user:
+		return jsonify({'message' : 'No user found!'})
+
+	db.session.delete(user)
+	db.session.commit()
+
+	return jsonify({'message' : 'User has been deleted!'})
 
 if __name__ == '__main__':
 	app.run(debug=True)
